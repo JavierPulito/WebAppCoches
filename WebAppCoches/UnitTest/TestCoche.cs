e9ea221cf96576b0842b487c94537db7e139d520
javipulitor <javipulitor@hotmail.com>
2022-02-01 00:04:37 -0800

    Terminado

--- WebAppCoches/UnitTest/TestCoche.cs
+++ WebAppCoches/UnitTest/TestCoche.cs
@@ -1,6 +1,31 @@
-﻿namespace WebAppCoches.UnitTest
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAppCoches.Model;
+using WebAppCoches.Repository;
+
+namespace WebAppCoches.UnitTest
 {
+    [TestClass]
     public class TestCoche
     {
+        private CocheModel Coche;
+        private RuedaModel Rueda;
+
+        private List<CocheModel> Coches = new List<CocheModel>();
+
+        private MiDBContext MiDBContext;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.Coche = new CocheModel();
+            this.Rueda = new RuedaModel(0, true);
+        }
+
+        [TestMethod]
+        public void TestAddCoche()
+        {
+            Assert.AreNotEqual(3,0);
+           
+        }
     }
 }
